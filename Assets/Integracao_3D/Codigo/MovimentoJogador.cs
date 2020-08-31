d9177d6462862e747787b6f47e57238eafef6161
Felipe Magno <philippos.magnum@gmail.com>
2020-08-31 20:29:34 -0300

    Meio da aula

---
+++ Assets/Integracao_3D/Codigo/MovimentoJogador.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimentoJogador : MonoBehaviour
+{
+    [SerializeField]
+    private Rigidbody meuRB;
+
+    [HideInInspector]
+    public float forca = 450f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (meuRB == null)
+        {
+            meuRB = GetComponent<Rigidbody>();
+            if (meuRB == null)
+            {
+                Debug.LogError("Deu ruim " + gameObject.name);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
