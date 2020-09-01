6a7e934a7fe7b996522f281e8a1cb456b1a1623a
Felipe Magno <philippos.magnum@gmail.com>
2020-09-01 13:13:51 -0300

    final da aula

--- Assets/Integracao_3D/Codigo/MovimentoJogador.cs
+++ Assets/Integracao_3D/Codigo/MovimentoJogador.cs
@@ -6,10 +6,15 @@ public class MovimentoJogador : MonoBehaviour
 {
     [SerializeField]
     private Rigidbody meuRB;
+    private Transform meuTransform;
 
-    [HideInInspector]
-    public float forca = 450f;
+    [SerializeField, Tooltip("controle da força aplicada ao movimento")]
+    private float forca = 450f;
+    [SerializeField, Tooltip("velocidade maxima em m/s")]
+    private float velocidadeMaxima = 5f;
 
+    private float vertical;
+    private float horizontal;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,11 +26,31 @@ public class MovimentoJogador : MonoBehaviour
                 Debug.LogError("Deu ruim " + gameObject.name);
             }
         }
+
+        meuTransform = transform;
     }
 
     // Update is called once per frame
     void Update()
     {
+        vertical = Input.GetAxis("Vertical") * forca;
+        horizontal = Input.GetAxis("Horizontal") * forca;
+
         
     }
+
+    private void FixedUpdate()
+    {
+        Vector3 frente = Vector3.forward * vertical;
+        Vector3 direita = Vector3.right * horizontal;
+        Vector3 forcaResultante = (frente + direita).normalized * Time.fixedDeltaTime * 100;
+
+        meuRB.AddForce(forcaResultante, ForceMode.VelocityChange);
+
+        //meuTransform.Translate(forcaResultante);
+
+        // Controla limite de velocidade total (inclui gravidade)   
+        meuRB.velocity = Vector3.ClampMagnitude(meuRB.velocity, velocidadeMaxima);
+    }
+
 }
